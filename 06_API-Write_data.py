81c0ba3bd62c70570165f627ee5c10218a5fecbb
Sreeram Ravishankar <sreeramtkd@hotmail.com>
2023-03-11 20:18:26 +0530

    Update 06_API-Write_data.py

--- 06_API-Write_data.py
+++ 06_API-Write_data.py
@@ -3,5 +3,5 @@ import time
 import Adafruit_DHT as dht
 while True:
     humidity,temperature=dht.read_retry(dht.DHT11,4)
-    requests.get('https://api.thingspeak.com/update?api_key=AJG9J825FVJDSQG2&field1='+str(temperature)+'field2='+str(humidity))
-    time.sleep(60)
+    requests.get('https://api.thingspeak.com/update?api_key=AJG9J825FVJDSQG2&field1='+str(temperature)+'&field2='+str(humidity))
+    time.sleep(15)
