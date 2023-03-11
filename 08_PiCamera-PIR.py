f297a43bfab494b78d7c2599daef5149c047111d
Sreeram Ravishankar <sreeramtkd@hotmail.com>
2023-03-11 20:44:30 +0530

    Create 08_PiCamera-PIR.py

---
+++ 08_PiCamera-PIR.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as gpio
+from picamera import PiCamera
+import time
+pir=4
+i=0
+gpio.setmode(gpio.BCM)
+gpio.setup(pir,gpio.IN)
+
+while True:
+	while gpio.input(pir):
+		camera = PiCamera()
+		while (i<=2):
+			camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+			print("Capturing...",i)
+			time.sleep(3)
+			i=i+1
+		camera.close()
+		i=0
+		time.sleep(10)
+
+#camera.start_recording('/home/pi/Desktop/video.h264')
+#sleep(5)
+#camera.stop_recording()
+
+
