fcde21728aea877013e1365b810078321c8fa924
Sreeram Ravishankar <sreeramtkd@hotmail.com>
2023-03-11 19:54:56 +0530

    Create 02_pir.py
    Detects state change of GPIO which has a PIR sensor connected to GPIO 4

---
+++ 02_pir.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+pir=4
+GPIO.setup(pir, GPIO.IN)
+
+while True:
+    if(GPIO.input(pir)):
+        print("Motion Detected !")
+    else:
+        print("Watching...")
+    time.sleep(0.5)
