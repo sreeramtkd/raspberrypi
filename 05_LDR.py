b5e9dcd8b5c0e04efd7d9df420b73d44d3b0ad3d
Sreeram Ravishankar <sreeramtkd@hotmail.com>
2023-10-22 17:49:30 +0530

    Update 05_LDR.py

--- 05_LDR.py
+++ 05_LDR.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-ldr=18
+ldr=18 #Measure the resistance of LDR and use ~ 1.5 times less its resistor value and connect that resistor to GPIO18 & Ground
 GPIO.setup(ldr, GPIO.IN)
 
 while True:
