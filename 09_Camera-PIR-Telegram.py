68bd86a6edfe3117ed8c7de9c2d9dc99bbfe36d2
Sreeram Ravishankar <sreeramtkd@hotmail.com>
2023-10-22 19:32:13 +0530

    Update 09_Camera-PIR-Telegram.py

--- 09_Camera-PIR-Telegram.py
+++ 09_Camera-PIR-Telegram.py
@@ -5,7 +5,7 @@ import telepot
 from telepot.loop import MessageLoop
 bot=telepot.Bot('**********************YOUR API KEY******************')
 user='' # your telegram userid, get it form userinfobot
-pir=4
+pir=23
 i=0
 gpio.setmode(gpio.BCM)
 gpio.setup(pir,gpio.IN)
@@ -13,11 +13,11 @@ gpio.setup(pir,gpio.IN)
 while True:
 	while gpio.input(pir):
 		camera = PiCamera()
-		while (i<=2):
+		while (i<=4):
 			camera.capture('/home/pi/Desktop/image%s.jpg' % i)
 			print("Capturing...",i)
 			bot.sendPhoto(user, photo=open('/home/pi/Desktop/image%s.jpg' % i, 'rb'))
-			time.sleep(3)
+			time.sleep(1)
 			i=i+1
 		camera.close()
 		i=0
