b12fcfd35dfcc548d04344f2e024b12fdf696ea4
Sreeram Ravishankar <sreeramtkd@hotmail.com>
2023-11-18 10:54:36 +0530

    Create 12_Camera_Gallery.py

---
+++ 12_Camera_Gallery.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as gpio
+from picamera import PiCamera
+import time
+pir=23
+i=0
+gpio.setmode(gpio.BCM)
+gpio.setup(pir,gpio.IN)
+
+while True:
+	while gpio.input(pir):
+		camera = PiCamera()
+		while (i<=4):
+			camera.capture('/var/www/html/img/image%s.jpg' % i)
+			print("Capturing...",i)
+			time.sleep(.5)
+			i=i+1
+		camera.close()
+		i=0
+		time.sleep(10)
